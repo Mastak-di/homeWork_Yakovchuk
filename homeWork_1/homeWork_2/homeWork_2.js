81013652ea0fb76cfdf9cf5e0d94f83cfaaf8e26
Mastak <dymastak@gmail.com>
2020-07-23 17:43:54 +0300

    new change

--- homeWork_1/homeWork_2/homeWork_2.js
+++ homeWork_1/homeWork_2/homeWork_2.js
@@ -1,1 +1,34 @@
-console.log('check');
+alert('Hello, take two numbers from 1 to 99. The first number must be less than the second number');
+let N = prompt('enter the first number');
+let M = prompt('enter the second number');
+console.log('answer:', N);
+console.log('answer:', M);
+let sum = 0;
+if(N>M){//проверяем не больше ли первое чило второго числа
+    alert('the first number bigger or first number');
+    }
+if(N==NaN || M==NaN){
+    alert('this is non number')
+}
+const typeNumber = confirm('even numbers skip?')//пропускаем ли четные числа
+if(typeNumber){
+    for(let i=N; i<M; i++){
+        while(i%2!==0){
+            sum+=i;
+    }
+}
+alert(sum);
+}
+else{
+    for(let i=N; i<M; i++){
+        sum+=i;
+    }
+    alert(sum);
+}
+
+
+
+
+
+
+
