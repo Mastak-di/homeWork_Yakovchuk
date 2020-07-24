9b7a6fb9dc5a342dd693e67cf31b71f66cb8e5af
Mastak <dymastak@gmail.com>
2020-07-24 22:11:02 +0300

    done HM#2

--- homeWork_1/homeWork_2/homeWork_2.js
+++ homeWork_1/homeWork_2/homeWork_2.js
@@ -1,43 +1,34 @@
 let sum = 0;
 alert('Hello, take two numbers from 1 to 99. The first number must be less than the second number');
 let N = prompt('enter the first number');//ввод первого числа
-console.log('answer:', N);
-if(isNaN(N)===true){//проверка, является ли первое число числом
-    alert('the first value is not number');
-    
-}
 let M = prompt('enter the second number');//ввод второго числа
+console.log('answer:', N);
 console.log('answer:', M);
-else if(isNaN(M)===true){//проверка, является ли второе число числом
-    alert('the second value is not number');
-    
+if(isNaN(N)===true){//проверка, является ли первое число числом
+    alert('the first value is not number'); 
 }
-
+else if(isNaN(M)===true){//проверка, является ли второе число числом
+    alert('the second value is not number');  
+}
 else if(N>M){//проверяем не больше ли первое чило второго числа
     alert('the first number bigger than the second number');
     }
     else{
         const typeNumber = confirm('even numbers skip?')//пропускаем ли четные числа
+        if(typeNumber===true){
+            for(let i=N; i<M; i++){
+                if(i%2){
+                    sum+=i;
+                }
+            }
+        alert('you want know sum of the numbers? Click ok');
+        alert(sum);
+        }
+        else{
+            for(let i=N; i<M; i++){
+                sum+=i; 
+            }
+            alert('you want know sum of the numbers? Click ok');
+            alert(sum);
+        }
     }
-    /*const typeNumber = confirm('even numbers skip?')//пропускаем ли четные числа
-if(typeNumber===true){
-    for(let i=N; i<M; i++){
-        while(i%2!==0){
-            sum+=i;
-    }
-}
-console.log(sum);//здесь должна выводиться сумма чисел от N до M без парных чисел
-}
-else{
-    for(let i=N; i<M; i++){
-        sum+=i;
-    }
-    console.log(sum);//здесь должна выводиться сумма всех чисел от N до M
-}*/
-
-
-
-
-
-
-
