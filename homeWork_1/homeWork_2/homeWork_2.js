ff1f8f0ca10767b100073775186487d4ed167dec
Mastak <dymastak@gmail.com>
2020-07-24 21:37:41 +0300

    change

--- homeWork_1/homeWork_2/homeWork_2.js
+++ homeWork_1/homeWork_2/homeWork_2.js
@@ -1,19 +1,25 @@
+let sum = 0;
 alert('Hello, take two numbers from 1 to 99. The first number must be less than the second number');
 let N = prompt('enter the first number');//ввод первого числа
 console.log('answer:', N);
 if(isNaN(N)===true){//проверка, является ли первое число числом
     alert('the first value is not number');
+    
 }
 let M = prompt('enter the second number');//ввод второго числа
 console.log('answer:', M);
-if(isNaN(M)===true){//проверка, является ли второе число числом
+else if(isNaN(M)===true){//проверка, является ли второе число числом
     alert('the second value is not number');
+    
 }
-let sum = 0;
-if(N>M){//проверяем не больше ли первое чило второго числа
+
+else if(N>M){//проверяем не больше ли первое чило второго числа
     alert('the first number bigger than the second number');
     }
-    const typeNumber = confirm('even numbers skip?')//пропускаем ли четные числа
+    else{
+        const typeNumber = confirm('even numbers skip?')//пропускаем ли четные числа
+    }
+    /*const typeNumber = confirm('even numbers skip?')//пропускаем ли четные числа
 if(typeNumber===true){
     for(let i=N; i<M; i++){
         while(i%2!==0){
@@ -27,7 +33,7 @@ else{
         sum+=i;
     }
     console.log(sum);//здесь должна выводиться сумма всех чисел от N до M
-}
+}*/
 
 
 
