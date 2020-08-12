1c639e005eeabb7effc8afe03cec213aa9549527
Mastak <dymastak@gmail.com>
2020-08-12 13:34:28 +0300

    HW4 done

--- HomeWork_4/HW4.js
+++ HomeWork_4/HW4.js
@@ -1,11 +1,8 @@
-//HW4
-
 const students = ["Александр", "Игорь", "Алена", "Ира", "Алексей", "Света"];
 const themes = ["Дифференцированное уровнение", "Теория автоматов", "Алгоритм и структура данных"];
 const marks = [4, 5, 5, 3, 4, 5];
 const doubleStudents = [];
-const giveTheme = [];
-const assessmentEach = [];
+let giveTheme = [];
 const giveMarks = [];
 
 function twoStudents(){
@@ -17,39 +14,34 @@ doubleStudents.push([students[3], students[4]]);
 console.log('The first task:', twoStudents());
 
 function themeForStudents(){
-  giveTheme.push(doubleStudents.slice(0, 1), themes.slice(0, 1));
-  giveTheme.push(doubleStudents.slice(1, 2), themes.slice(1, 2));
-  giveTheme.push(doubleStudents.slice(2), themes.slice(2));
-
+  
+  for(let i=0; i<doubleStudents.length; i++){
+    giveTheme[i] = [doubleStudents[i].join(" и "), themes[i]];
+  }
 return giveTheme;
 }
 console.log('The second task:', themeForStudents());
-  
-function assessmentEveryone(){
-  assessmentEach.push(students.slice(0, 1), marks.slice(0, 1));
-  assessmentEach.push(students.slice(0, 1), marks.slice(1, 2));
-  assessmentEach.push(students.slice(0, 1), marks.slice(2, 3));
-  assessmentEach.push(students.slice(0, 1), marks.slice(3, 4));
-  assessmentEach.push(students.slice(0, 1), marks.slice(4, 5));
-  assessmentEach.push(students.slice(0, 1), marks.slice(5));
-  return assessmentEach;
+
+function markForStudents(){
+  let assessmentEach = [];
+  for(let i=0; i<students.length; i++){
+    assessmentEach[i] = [students[i], marks[i]];
+  }
+return assessmentEach;
 }
- console.log('The third task:', assessmentEveryone()); 
+console.log('The third task:', markForStudents());
 
-function randomAssessment(a, b) {
-  let assessment = Math.floor(Math.random()* (a - b)) + b;
-  giveMarks.push(doubleStudents.slice(0, 1), themes.slice(0, 1), assessment);
-  let assessment1 = Math.floor(Math.random()* (a - b)) + b;
-  giveMarks.push(doubleStudents.slice(1, 2), themes.slice(1, 2), assessment1);
-  let assessment2 = Math.floor(Math.random()* (a - b)) + b;
-  giveMarks.push(doubleStudents.slice(2), themes.slice(2), assessment2);
-          return giveMarks;
+function randomeNumbers(a=1, b=5){
+  let assessment = Math.floor(Math.random()* (b - a)) + a;
+  return assessment;
 }
-console.log('The fourth task:', randomAssessment(1, 5));
+console.log(randomeNumbers());
 
-
-
-
-
-
-
+function randomAssessment() {
+  let getAssessmentPair = [];
+  for(let i=0; i<giveTheme.length; i++){
+    getAssessmentPair[i] = [giveTheme[i], randomeNumbers()];
+  }
+  return getAssessmentPair;
+}
+console.log('The fourth task:', randomAssessment());
