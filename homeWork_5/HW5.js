24270b307251b80a421b32d5f8c61b9525557bee
Mastak <dymastak@gmail.com>
2020-08-24 21:46:38 +0300

    fixed my home work

--- homeWork_5/HW5.js
+++ homeWork_5/HW5.js
@@ -1,45 +1,49 @@
-const fifteenNumbers = [];
-function getRandomNumbers(min, max){
-    let randomNumbers = Math.floor(Math.random()* (max - min)) + min;
+const manyNumbers = [];
+function getRandomNumbers(){
+  const length = 15;
+  const min = -10;
+  const max = 100;
+  const randomNumbers = Math.floor(Math.random()* (max - min)) + min;
   return randomNumbers;
 }
+
 //task 1
 function getRandomArray(length, min, max){
-  for(let i=0; i<length; i++){
-    fifteenNumbers.push(getRandomNumbers(15, -10, 50));
+    for(let i=0; i<length; i++){
+    manyNumbers.push(getRandomNumbers(length, min, max));
   }
-  return fifteenNumbers;
+  return manyNumbers;
 }
-console.log("Array on fifteen numbers:", getRandomArray(15, 1, 100));
+console.log("Array on fifteen numbers:", getRandomArray(15, -10, 100));
 
 //task 2
 function getAverage(){
   let sumNumbers = 0;
-      for(let i=0; i<fifteenNumbers.length; i++){
-        sumNumbers += fifteenNumbers[i];   
+      for(let i=0; i<manyNumbers.length; i++){
+        sumNumbers += manyNumbers[i];   
   }
-  let averageNumber = Math.floor(sumNumbers/fifteenNumbers.length);
+  let averageNumber = Math.floor(sumNumbers/manyNumbers.length);
   return averageNumber;
 }
 console.log("Average random numbers is:", getAverage());
 
 //task 3
 function filterEvenNumbers() {
-     let filterEven = fifteenNumbers.filter((num) => num % 2 !== 0);
+     let filterEven = manyNumbers.filter((num) => num % 2 !== 0);
   return filterEven;
   }
 console.log("Array without paired numbers:", filterEvenNumbers());
 
 //task4
 function countPositiveNumbers() {
-     let filterBiggerZero = fifteenNumbers.filter((notZero) => notZero > 0);
+     let filterBiggerZero = manyNumbers.filter((notZero) => notZero > 0);
   return filterBiggerZero;
   }
 console.log("Array numbers bigger zero:", countPositiveNumbers());
 
 //task 5
 function getDividedByFive() {
-     let filterDivide = fifteenNumbers.filter((onFive) => onFive % 5 === 0);
+     let filterDivide = manyNumbers.filter((onFive) => onFive % 5 === 0);
   return filterDivide;
   }
 console.log("Numbers which divide on five:", getDividedByFive());
@@ -55,4 +59,4 @@ function divideByThree(word){
     }
   return cutWord;
 }
-console.log(divideByThree("Commander"));
+console.log("divide the word:", divideByThree("Commander"));
